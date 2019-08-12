6b795111fe68e3c890d12f2062514fc00a1f9760
Jolie-G <joliegasconhumanfactors@gmail.com>
2019-08-12 16:24:20 -0400

    employee service

---
+++ challenge-employee-service/src/main/java/com/example/DAO/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.example.DAO;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.beans.Employee;
+
+
+	@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// other choices besides JpaRepository as parent type - also have CrudRepository (supertype)
+	// and PagingAndSortingRepository
+
+	// method name pattern matching
+public  List<Employee> getAllEmployee ();
+
+
+	}
