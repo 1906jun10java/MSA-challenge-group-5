0d7cac14e687f2262fa3c9494e6e9d06b8998668
tayoung3 <tyoung3@umassd.edu>
2019-08-13 19:06:13 -0400

    config update

--- ChallengeEmployee/src/main/java/com/revature/controller/EmployeeController.java
+++ ChallengeEmployee/src/main/java/com/revature/controller/EmployeeController.java
@@ -3,17 +3,19 @@ package com.revature.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.service.EmployeeService;
 import com.revature.bean.Employee;
+import com.revature.service.EmployeeService;
 
 
 @RestController
+@RefreshScope
 @RequestMapping(value="/employee")
 public class EmployeeController {
 
