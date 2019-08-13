df2dc3df0b079aca90df890f9529425148963875
tayoung3 <tyoung3@umassd.edu>
2019-08-13 14:58:35 -0400

    sneaking suspicion

--- ChallengeReimbursement/src/main/java/com/example/controller/ReimbursementController.java
+++ ChallengeReimbursement/src/main/java/com/example/controller/ReimbursementController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bean.Reimbursement;
@@ -23,7 +23,7 @@ public class ReimbursementController {
 		this.reimbursementService = reimbursementService;
 	}
 	
-	@GetMapping(value="/all")
+	@RequestMapping(value="/all", method=RequestMethod.GET)
 	public ResponseEntity<List<Reimbursement>> getAllReimbursements() {
 		return new ResponseEntity<>(reimbursementService.allReimbursements(), HttpStatus.OK);
 	}
--- ChallengeReimbursement/src/main/java/com/example/dao/ReimbursementDAO.java
+++ ChallengeReimbursement/src/main/java/com/example/dao/ReimbursementDAO.java
@@ -1,14 +1,11 @@
 package com.example.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.bean.Reimbursement;
 
 @Repository
-public interface ReimbursementDAO extends JpaRepository<Reimbursement, Integer>{
+public interface ReimbursementDAO extends JpaRepository<Reimbursement, Integer> {
 
-	public List<Reimbursement> getAllReimbursement();
 }
